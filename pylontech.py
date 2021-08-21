cb2b010e1fe56cd3c68616504bf93b919a3c60ed
Frank Villaro-Dixon <frank@vi-di.fr>
2021-08-22 00:07:38 +0200

    mind: blown

--- pylontech.py
+++ pylontech.py
@@ -1,33 +1,30 @@
+from construct.core import FormatField
 import serial
 import construct
 
 class HexToByte(construct.Adapter):
     def _decode(self, obj, context, path) -> bytes:
-        # called at building time to return a modified version of obj
+        # XXX This is pure pood
         hexstr = [chr(x) for x in obj]
-        #print('hello')
-        #print(hexstr)
         hex2 = [chr(int(''.join(hexstr[i:i+2]), 16)) for i in range(0, len(hexstr), 2)]
-        #hex2 = [chr(int(hexstr[i:i+2], 16)) for i in range(0, len(hexstr), 2)]
-        return ''.join(hex2).encode()
-
-class HexToInt(construct.Adapter):
-    def _decode(self, obj, context, path) -> bytes:
-        hexstr = [chr(x) for x in obj]
-
-        #print('hello')
-        #print(hexstr)
-        hex2 = [chr(int(''.join(hexstr[i:i+2]), 16)) for i in range(0, len(hexstr), 2)]
-        #hex2 = [chr(int(hexstr[i:i+2], 16)) for i in range(0, len(hexstr), 2)]
         return ''.join(hex2).encode()
 
 class JoinBytes(construct.Adapter):
     def _decode(self, obj, context, path) -> bytes:
-        print(obj)
         return ''.join([chr(x) for x in obj]).encode()
+
+class DivideBy1000(construct.Adapter):
+    def _decode(self, obj, context, path) -> float:
+        return obj / 1000
+
+class ByteArrayToVolt(construct.Adapter):
+    def _decode(self, obj, context, path) -> float:
+        raise Exception('unimpl')
+
 
 
 class Pylontech:
+    # XXX Array(10, Bytes) -> Byte(10)
     manufacturer_info_fmt = construct.Struct(
         "DeviceName" / JoinBytes(construct.Array(10, construct.Byte)),
         "SoftwareVersion" / construct.Array(2, construct.Byte),
@@ -35,11 +32,14 @@ class Pylontech:
     )
 
     system_parameters_fmt = construct.Struct(  # XXX Yields invalid parsing
-        "CellHighVoltageLimit" / construct.Array(2, construct.Byte),
-        "CellLowVoltageLimit" / construct.Array(2, construct.Byte),
-        "CellUnderVoltageLimit" / construct.Array(2, construct.Byte),
-        "ChargeHighTemperatureLimit" / construct.Array(2, construct.Byte),
-        "ChargeLowTemperatureLimit" / construct.Array(2, construct.Byte),
+        "CellHighVoltageLimit" / construct.Int16sb,
+        #"CellHighVoltageLimit" / construct.Array(2, construct.Byte),
+        "CellLowVoltageLimit" / construct.Int16sb,
+        #"CellLowVoltageLimit" / construct.Array(2, construct.Byte),
+        "CellUnderVoltageLimit" / construct.Int16sb,
+        #"CellUnderVoltageLimit" / construct.Array(2, construct.Byte),
+        "ChargeHighTemperatureLimit" / construct.Int16sb,
+        "ChargeLowTemperatureLimit" / construct.Int16sb,
         "ChargeCurrentLimit" / construct.Array(2, construct.Byte),
         "ModuleHighVoltageLimit" / construct.Array(2, construct.Byte),
         "ModuleLowVoltageLimit" / construct.Array(2, construct.Byte),
@@ -47,13 +47,44 @@ class Pylontech:
         "DischargeHighTemperatureLimit" / construct.Array(2, construct.Byte),
         "DischargeLowTemperatureLimit" / construct.Array(2, construct.Byte),
         "DischargeCurrentLimit" / construct.Array(2, construct.Byte),
+        "GREEDY" / JoinBytes(construct.GreedyRange(construct.Byte)),
+    )
+
+    management_info_fmt = construct.Struct(  # XXX Yields invalid parsing
+        "CommandValue" / construct.Byte,
+        "ChargeVoltageLimit" / construct.Array(2, construct.Byte),
+        "DischargeVoltageLimit" / construct.Array(2, construct.Byte),
+        "ChargeCurrentLimit" / construct.Array(2, construct.Byte),
+        "DishargeCurrentLimit" / construct.Array(2, construct.Byte),
+        "Status" / construct.Byte,
+    )
+
+    module_serial_number_fmt = construct.Struct(  # XXX Yields invalid parsing
+        "CommandValue" / construct.Array(1, construct.Byte),
+        "Dummy" / construct.Array(2, construct.Byte),
+        "ModuleSerialNumber" / JoinBytes(construct.Array(14, construct.Byte)),
+    )
+
+
+    get_values_fmt = construct.Struct(  # XXX Yields invalid parsing
+        "CommandValue" / construct.Byte,
+        "NumberOfCells" / construct.Byte,
+        "CellVoltages" / construct.Array(construct.this.NumberOfCells, construct.Array(3, construct.Byte)),
+        "NumberOfTemperatures" / construct.Byte,
+        "AverageBMSTemperature" / construct.Array(3, construct.Byte),
+        "GroupedCellsTemperatures" / construct.Array(construct.this.NumberOfTemperatures-1, construct.Array(3, construct.Byte)),
+        "Current" / construct.Int16ub,
+        "Voltage" / DivideBy1000(construct.Int16ub),
+        "FOOBAR" / construct.GreedyRange(construct.Byte),
     )
 
     def __init__(self):
         port = '/dev/ttyUSB0'
         self.s = serial.Serial(port, 115200, bytesize=8, parity=serial.PARITY_NONE, stopbits=1, timeout=2)
 
-    def get_frame_checksum(self, frame: bytes):
+
+    @staticmethod
+    def get_frame_checksum(frame: bytes):
         assert isinstance(frame, bytes)
 
         sum = 0
@@ -65,23 +96,39 @@ class Pylontech:
         sum += 1
         return sum
 
+    @staticmethod
+    def get_info_length(info: bytes) -> int:
+        #print('HELLO')
+        lenid = len(info)
+        if lenid == 0:
+            return 0
+        #print(f'LENID: {lenid}: {bin(lenid)}')
 
-    def send_cmd(self, address: int, cmd, info):
+        lenid_sum = (lenid & 0xf) + ((lenid >> 4) & 0xf) + ((lenid >> 8) & 0xf)
+        #print(f'LSUM: {lenid_sum}: {bin(lenid_sum)}')
+        lenid_modulo = lenid_sum % 16
+        #print(f' MOD: {lenid_modulo} - {bin(lenid_modulo)}')
+        lenid_invert_plus_one = 0b1111 - lenid_modulo + 1
+        #print(f'invert plus one: {lenid_invert_plus_one}: {bin(lenid_invert_plus_one)}')
+
+        return (lenid_invert_plus_one << 12) + lenid
+
+
+    def send_cmd(self, address: int, cmd, info: bytes = b''):
         raw_frame = self._encode_cmd(address, cmd, info)
         print(f">> {raw_frame}")
         self.s.write(raw_frame)
 
 
-    def _encode_cmd(self, address: int, cid2: int, info):
+    def _encode_cmd(self, address: int, cid2: int, info: bytes = b''):
         cid1 = 0x46
-        if len(info) != 0:
-            raise Exception('Unimplemented for now')
 
-        info_length = 0
+        info_length = Pylontech.get_info_length(info)
 
         frame = "{:02X}{:02X}{:02X}{:02X}{:04X}".format(0x20, address, cid1, cid2, info_length).encode()
+        frame += info
 
-        frame_chksum = self.get_frame_checksum(frame)
+        frame_chksum = Pylontech.get_frame_checksum(frame)
         whole_frame = (b"~" + frame + "{:04X}".format(frame_chksum).encode() + b"\r")
         return whole_frame
 
@@ -91,7 +138,7 @@ class Pylontech:
         frame_data = raw_frame[1:len(raw_frame)-5]
         frame_chksum = raw_frame[len(raw_frame)-5:-1]
 
-        got_frame_checksum = self.get_frame_checksum(frame_data)
+        got_frame_checksum = Pylontech.get_frame_checksum(frame_data)
         assert got_frame_checksum == int(frame_chksum, 16)
 
         return frame_data
@@ -117,30 +164,67 @@ class Pylontech:
 
 
     def get_protocol_version(self):
-        # XXX TODO
-        self.send_cmd(0, 0x4f, '')
-        rea = self.s.readline()
-        print(f"<< {rea}")
+        self.send_cmd(0, 0x4f)
+        f = self.read_frame()
+        return f.ver
 
 
     def get_manufacturer_info(self):
-        self.send_cmd(0, 0x51, '')
+        self.send_cmd(0, 0x51)
         f = self.read_frame()
-
         ff = self.manufacturer_info_fmt.parse(f.info)
-        print(ff)
+        return ff
 
 
     def get_system_parameters(self):
-        self.send_cmd(2, 0x47, '')
+        self.send_cmd(2, 0x47)
         f = self.read_frame()
-        print(f)
-        print(f.info)
 
-        ff = self.system_parameters_fmt.parse(f.info)
+        print(f.info[1:])
+        ff = self.system_parameters_fmt.parse(f.info[1:])
         print(ff)
+        return ff
+
+    def get_management_info(self):
+        raise Exception('Dont touch this for now')
+        self.send_cmd(2, 0x92)
+        f = self.read_frame()
+
+        print(f.info)
+        print(len(f.info))
+        ff = self.management_info_fmt.parse(f.info)
+        print(ff)
+        return ff
+
+    def get_module_serial_number(self):
+        self.send_cmd(2, 0x93)
+        f = self.read_frame()
+
+        ff = self.module_serial_number_fmt.parse(f.info)
+        print('FOO')
+        print(ff.ModuleSerialNumber)
+        return ff
+
+    def get_values(self):
+        self.send_cmd(2, 0x42, b'\x01')
+        f = self.read_frame()
+
+        print(f.info[1:])
+
+        infoflag = f.info[0]
+        d = self.get_values_fmt.parse(f.info[1:])
+        print(d)
+        return None
 
 
 if __name__ == '__main__':
     p = Pylontech()
-    p.get_system_parameters()
+    #print(p.get_protocol_version())
+    #print(p.get_manufacturer_info())
+    print(p.get_system_parameters()) #  # XXX TO RETRY (INFOFLAG)
+    #p.get_management_info()
+    #p.get_module_serial_number()
+    #p.get_values()
+    #il = Pylontech.get_info_length(b'111111111111111111')
+    #print(il)
+    #print(bin(il))
