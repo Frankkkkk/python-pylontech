3627510242111f51658ccf11f457067429467600
Frank Villaro-Dixon <frank@vi-di.fr>
2021-08-30 23:39:12 +0200

    New version 0.0.4

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 
 setup(
     name="python-pylontech",
-    version="0.0.3",
+    version="0.0.4",
     author="Frank Villaro-Dixon",
     author_email="frank@villaro-dixon.eu",
     description=("Interfaces with Pylontech Batteries using RS485 protocol"),
